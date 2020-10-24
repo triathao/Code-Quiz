e88649367290d798a2adc6b057899ccd9f9abf5b
Tria Thao <triathao@gmail.com>
2020-10-23 17:17:11 -0700

    fix errors

--- Assets/app.js
+++ Assets/app.js
@@ -6,6 +6,7 @@ const optionContainer = document.querySelector(".option-container");
 let questionCounter = 0;
 let currentQuestion;
 let availableQuestions = [];
+let availableOptions = [];
 
 // Push the questions into availableQuestions Array
 function setAvailableQuestions (){
@@ -25,17 +26,44 @@ function getNewQuestion(){
     const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
     currentQuestion = questionIndex;
     questionText.innerHTML = currentQuestion.q;
+    //get the position of 'question' from the availableQuestion Array
+    const index1= availableQuestions.indexOf(questionIndex);
+    // remove the 'questionIndex' fromt he availableQuestion Array, so that the question does not repeat
+    availableQuestions.splice(index1,1);
     //console.log(questionIndex)
+    //console.log(availableQuestions)
 
-    questionCounter++
-    
+    // set options
+    // get the length of options
+    const optionLen = currentQuestion.options.length
+    //push options into availableOptions Array
+    for(let i=0; i<optionLen; i++){
+        availableOptions.push(i)
+    }
+    // creat options in html
+    for(let i=0; i<optionLen; i++){
+        //random option
+        const optionIndex = availableOptions[Math.random() * availableOptions.length];
+        // get the position of 'optionIndex' from the availableOptions
+        const index2 = availableOptions.indexOf(optionIndex);
+        // remove the 'optionIndex' from the availableOptions,
+        availableOptions.splice(index2,1)
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[i];
+        option.id = i;
+        option.className = "option";
+        optionContainer.appendChild(option)
+
+    }
+
+    questionCounter++   
 }
    
 function next(){
     if(questionCounter === quiz.length){
         console.log("quiz over");
     }
-    esle{
+    else {
         getNewQuestion();
     }
 }
